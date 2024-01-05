922f67d7d307adb11ec0ec99b2e9e1a7bceecdf0
Sfa11eN <sfallenn@gmail.com>
2024-01-05 19:04:37 +0300

    just commit

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,29 @@ public class Main {
             System.out.printf("Месяц %d, сумма накоплений равна %.2f рублей%n", month, total);
             month++;
         }
+        System.out.println("Задание 2");
+
+        int counter = 1;
+
+        while (counter <= 10) {
+            System.out.println(counter + " ");
+            counter++;
+        }
+
+        System.out.println();
+
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i+" ");
+        }
+        System.out.println("Задание 3");
+        int humanPopulation=12000000;
+        float countBirth=(float) 17/1000;
+        float countDeath=(float) 8/1000;
+        for (int year=1; year<11; year++){
+            humanPopulation=(int) (humanPopulation+(humanPopulation*countBirth)-(humanPopulation*countDeath));
+            System.out.printf("Год %d, численность населения составляет %d%n " ,year,humanPopulation);
+        }
+
     }
 }
